be3415161afd965ac727a8c1a7c4663046b025a6
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2024-09-11 10:58:37 +0700

    Update EnderBoxEntity.php

--- src/Nozell/Crates/Entity/EnderBoxEntity.php
+++ src/Nozell/Crates/Entity/EnderBoxEntity.php
@@ -73,7 +73,7 @@ class EnderBoxEntity extends Living {
                         $meeting->reduceKeyEnder();
                         Main::getInstance()->getCrateManager()->getRandomItemFromCrate("ender", $damager->getName(), $this);
                     } else {
-                        $damager->sendMessage("§cAl parecer no tienes keys!");
+                        $damager->sendMessage("§cYour key not enough to open!");
                     }
                 }
             }
